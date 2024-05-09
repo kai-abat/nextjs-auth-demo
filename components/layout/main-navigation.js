ef53c0db198a66405d26ff8f968fbb6309f1ad5e
Chaim Abat <kai.balsamo.abat@gmail.com>
2024-05-09 11:14:15 +0800

    update colors

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import classes from './main-navigation.module.css';
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
   return (
     <header className={classes.header}>
       <Link href="/">
-        <div className={classes.logo}>Next Auth</div>
+        <div className={classes.logo}>Next Auth Demo</div>
       </Link>
       <nav>
         <ul>
